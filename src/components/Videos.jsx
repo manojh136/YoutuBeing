94a038eaa31b2a215b50658b912224eecd3643bc
manojh136 <58912761+manojh136@users.noreply.github.com>
2023-08-04 19:04:07 +0530

    Add files via upload

---
+++ src/components/Videos.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return <Loader />;
+  // console.log(videos);
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => {
+        if(item.id.videoId || item.id.channelId){
+          return(
+            <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+          );
+        }
+      })}
+    </Stack>
+  );
+}
+
+export default Videos;
